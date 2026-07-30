713a278d030b5bf8533315a766e52a6651a526fe
Alexis Joel Dávila <alexis19031992@gmail.com>
2026-07-30 17:09:26 -0300

    fix(steamcmd): valida correctamente el bootstrap instalado

--- src/shared/constants/app-info.ts
+++ src/shared/constants/app-info.ts
@@ -6,7 +6,7 @@ export const APP_INFO = {
   appId: 'com.palcm.servermanager',
   authorAlias: '>GR477<',
   authorName: 'Alexis Joel Dávila',
-  version: '0.17.1',
+  version: '0.17.2',
   channel: 'Dev'
 } as const;
 
